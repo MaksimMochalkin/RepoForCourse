5b5bbbacf7aa96a9dd5402773273454e5e50cfab
Maxim Mochalkin <maksimmochalkin@gmail.com>
2021-11-24 20:29:54 +0600

    finished Task 2

--- Reflection_Task2/ObjectExtensions.cs
+++ Reflection_Task2/ObjectExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace Task2
 {
@@ -6,12 +9,47 @@ namespace Task2
     {
         public static void SetReadOnlyProperty(this object obj, string propertyName, object newValue)
         {
-            throw new NotImplementedException();
+            FieldInfo backingField = null;
+            var objType = obj.GetType();
+            var property = objType.GetProperty(propertyName);
+            var baseType = objType.BaseType;
+            if (property.Name == "Property" && baseType.Name != "Object")
+            {
+                backingField = baseType
+                  .GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)
+                  .FirstOrDefault();
+            }
+            else
+            {
+                backingField = objType
+                  .GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)
+                  .FirstOrDefault();
+            }
+
+            backingField.SetValue(obj, newValue);
         }
 
         public static void SetReadOnlyField(this object obj, string filedName, object newValue)
         {
-            throw new NotImplementedException();
+            FieldInfo backingField = null;
+            var objType = obj.GetType();
+            var field = objType.GetField(filedName);
+            var baseType = objType.BaseType;
+            if (field.Name == "Filed" && baseType.Name != "Object")
+            {
+                backingField = baseType
+                  .GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static)
+                  .FirstOrDefault(f => f.Name == filedName);
+            }
+            else
+            {
+                backingField = objType
+                  .GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static)
+                  .FirstOrDefault(f => f.Name == filedName);
+                  
+            }
+
+            backingField.SetValue(obj, newValue);
         }
     }
 }
