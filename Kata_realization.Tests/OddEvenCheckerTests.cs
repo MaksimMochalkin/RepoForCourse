97a38ce675092ba73b88f9f0366c8f223a28d582
Maxim Mochalkin <maksimmochalkin@gmail.com>
2021-12-14 11:17:17 +0600

    UnitTesting: add Katas implementation

---
+++ Kata_realization.Tests/OddEvenCheckerTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Kata_realization.Tests
+{
+    using NUnit.Framework;
+    using Shouldly;
+
+    public class OddEvenCheckerTests
+    {
+        private OddEvenChecker _checker;
+        [SetUp]
+        public void Setup()
+        {
+            _checker = new OddEvenChecker();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            _checker.isOddEvenRange(1,100);
+
+            _checker.isOddEvenDigit(1).ShouldBe(true);
+            _checker.isOddEvenDigit(9).ShouldBe(true);
+            _checker.isOddEvenDigit(10).ShouldBe(true);
+            _checker.isOddEvenDigit(11).ShouldBe(false);
+        }
+    }
+}
---
+++ Kata_realization.Tests/StringCalcTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Kata_realization.Tests
+{
+    using NUnit.Framework;
+    using Shouldly;
+    using System;
+
+    public class StringCalcTests
+    {
+        private StringCalc _calc;
+
+        [SetUp]
+        public void Setup()
+        {
+           _calc = new StringCalc();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // String empty, throw ArgumentNullException
+            var exception = Assert.Throws<ArgumentNullException>(() => _calc.Add(string.Empty));
+            Assert.That(exception.Message, Is.SupersetOf("String can not be null or empty"));
+
+            // String invalid format, throw FormatException
+            var ex = Assert.Throws<FormatException>(() => _calc.Add("1,\n"));
+            Assert.That(ex.Message, Is.SupersetOf("String contains an invalid sequence. Invalid format: 'number, separator, line feed.'"));
+            ex = Assert.Throws<FormatException>(() => _calc.Add("1,\n,3"));
+            Assert.That(ex.Message, Is.SupersetOf("String contains an invalid sequence. Invalid format: 'number, separator, line feed.'"));
+
+            // String contains negatives digits, throw ArgumentException
+            var e = Assert.Throws<ArgumentException>(() => _calc.Add("-1,5,-7"));
+            Assert.That(e.Message, Is.SupersetOf("Negatives not allowed. -1,-7"));
+
+            _calc.Add("1").ShouldBe(1);
+            _calc.Add("1,2").ShouldBe(3);
+            _calc.Add("1\n2").ShouldBe(3);
+            _calc.Add(";1\n2;3").ShouldBe(6);
+
+        }
+    }
+}
